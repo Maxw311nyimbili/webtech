155095f020a3513f10b77ff9ca3a9e2bb9ce37dd
Maxw311 <nyimbilimaxwell9@gmail.com>
2024-11-16 19:49:23 +0000

    changes

--- db/db_connection.php
+++ db/db_connection.php
@@ -31,42 +31,58 @@
 // }
 
 
-$servername = "169.239.251.102";
+$servername = "localhost";
 $username = "maxwell.nyimbili"; 
 $password = "L3gendary1864"; 
 $dbname = "webtech_fall2024_maxwell_nyimbili";
 $port = "3341";
 
-// PDO connection settings
-$dsn = "mysql:host=$servername;dbname=$dbname;port=$port;charset=utf8mb4";
+// // PDO connection settings
+// $dsn = "mysql:host=$servername;dbname=$dbname;port=$port;charset=utf8mb4";
 
 
-$options = [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Enable exceptions for error handling
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // Fetch results as associative arrays
-    PDO::ATTR_EMULATE_PREPARES => false, // Disable emulation of prepared statements (for better security)
-    PDO::ATTR_TIMEOUT => 10
-];
+// $options = [
+//     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Enable exceptions for error handling
+//     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // Fetch results as associative arrays
+//     PDO::ATTR_EMULATE_PREPARES => false, // Disable emulation of prepared statements (for better security)
+//     PDO::ATTR_TIMEOUT => 10
+// ];
 
-try {
-    // Create the PDO instance (database connection)
-    $pdo = new PDO($dsn, $username, $password, $options);
-    echo "Connected to the database successfully"; // Optional success message
-} catch (PDOException $e) {
-    // Catch connection errors and display a message
-    die('Could not connect to the database: ' . $e->getMessage());
+// try {
+//     // Create the PDO instance (database connection)
+//     $pdo = new PDO($dsn, $username, $password, $options);
+//     echo "Connected to the database successfully"; // Optional success message
+// } catch (PDOException $e) {
+//     // Catch connection errors and display a message
+//     die('Could not connect to the database: ' . $e->getMessage());
+// }
+
+// // Optional: Test the connection by executing a simple query
+// try {
+//     $pdo->query('SELECT 1');
+//     echo 'Connection is successful';
+// } catch (PDOException $e) {
+//     die('Connection failed: ' . $e->getMessage());
+// }
+
+
+// <?php
+
+// $db_host = 'localhost';
+// $db_user = 'root';
+// $db_pass = 'cs341webtech';
+// $db_name = 'AC2024';
+
+// Attempt to connect to the database
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
+
+// Check connection
+if ($conn -> connect_error) 
+{
+    echo''. $conn -> connect_error;
+    die("Connection failed: " . $conn->connect_error);
 }
-
-// Optional: Test the connection by executing a simple query
-try {
-    $pdo->query('SELECT 1');
-    echo 'Connection is successful';
-} catch (PDOException $e) {
-    die('Connection failed: ' . $e->getMessage());
-}
-
-
-
+echo "Connection successful";
 
 // $servername = "169.239.251.102";
 // $username = "maxwell.nyimbili"; 
